26c5e007383c5deb1f696444200c5add3cb53fba
daeik kim <12377dl@gmail.com>
2023-10-11 23:35:39 +0900

    .

--- ch05/03.listen_packet_test.go
+++ ch05/03.listen_packet_test.go
@@ -1,1 +1,38 @@
 package echo
+
+import (
+	"context"
+	"net"
+	"testing"
+)
+
+func TestListenPacketUDP(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	serverAddr, err := echoServerUDP(ctx, "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cancel()
+
+	client, err := net.ListenPacket("udp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() { _ = client.Close() }()
+
+	interloper, err := net.ListenPacket("udp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	interrupt := []byte("pardon me")
+	n, err := interloper.WriteTo(interrupt, client.LocalAddr())
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = interloper.Close()
+
+	if l := len(interrupt); l != n {
+		t.Fatalf("wrote %d bytes of %d", n, l)
+	}
+}
