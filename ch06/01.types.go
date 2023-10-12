1c2b025f2d2ee16a64f43db2d931148ab1983f11
daeik kim <12377dl@gmail.com>
2023-10-12 23:33:26 +0900

    .

---
+++ ch06/01.types.go
@@ -0,0 +1,78 @@
+package tftp
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+const (
+	DatagramSize = 516
+	BlockSize    = DatagramSize - 4
+)
+
+type OpCode uint16
+
+const (
+	OpRRQ OpCode = iota + 1
+	_
+	OpData
+	OpAck
+	OpErr
+)
+
+type ErrCode uint16
+
+const (
+	ErrUnknown ErrCode = iota
+	ErrNotFound
+	ErrAccessViolation
+	ErrDiskFull
+	ErrIllegalOp
+	ErrUnknownID
+	ErrFileExists
+	ErrNoUser
+)
+
+type ReadReq struct {
+	Filename string
+	Mode     string
+}
+
+func (q ReadReq) MarshalBinary() ([]byte, error) {
+	mode := "octet"
+	if q.Mode != "" {
+		mode = q.Mode
+	}
+
+	cap := 2 + 2 + len(q.Filename) + 1 + len(q.Mode) + 1
+
+	b := new(bytes.Buffer)
+	b.Grow(cap)
+
+	err := binary.Write(b, binary.BigEndian, OpRRQ)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = b.WriteString(q.Filename)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = b.WriteByte(0)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = b.WriteString(mode)
+	if err != nil {
+		return nil, err
+	}
+
+	err = b.WriteByte(0)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
